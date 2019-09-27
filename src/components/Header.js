9f71dddba97720b278909b3dca8ad9a1a9aa746d
Victor Fernandes <victor.fernandes.07@gmail.com>
2019-09-27 11:03:58 -0300

    Terminado

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,10 @@ function Header(){
       <nav>
         <img src="https://i.imgur.com/KDIDiSE.png"/>
         <div>
-          
+          <span>Meu Perfil</span>
+          <i class="material-icons">
+            account_circle
+          </i>
         </div>
       </nav>
     </header>
